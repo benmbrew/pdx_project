8800deb9bc6a75f4428adaf7b1f3dd4953b1413e
benmbrew <benmbrew@gmail.com>
2017-04-13 11:13:47 -0400

    champions oncology data

--- Scripts/champions_oncology/explore_data.R
+++ Scripts/champions_oncology/explore_data.R
@@ -20,4 +20,21 @@ code_folder <- paste0(project_folder, 'Code/')
 # Read in data
 ##########
 
-# 
+# rna_seq_expression
+rna <- read.csv(paste0(champ_onc_folder, '/rna_seq_expression.csv'))
+
+# cnv_export
+cnv <- read.csv(paste0(champ_onc_folder, '/cnv_export.csv'))
+
+# drugs
+drugs <- read.csv(paste0(champ_onc_folder, '/drugs.csv'))
+
+# fusions
+fusions <- read.csv(paste0(champ_onc_folder, '/fusions.csv'))
+
+# mutations_export
+mut <- read.csv(paste0(champ_onc_folder, '/mutations_export.csv'))
+
+# treatment_history
+treatment <- read.csv(paste0(champ_onc_folder, '/treatment_history.csv'))
+
